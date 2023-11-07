1c3f1c83f3b97dc514ffeaa1a10f4163e61108c3
shawn edgell <shawn_edgell@hotmail.com>
2023-11-07 14:19:52 -0500

    simple layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- src/routes/+page.svelte -->
+<h1 class="text-3xl font-bold text-center mt-6">Welcome to SuperChat</h1>
+<p class="text-center mt-4">
+  Visit <a href="https://kit.svelte.dev" class="text-blue-600 hover:underline">kit.svelte.dev</a> to read the documentation.
+</p>
+
+<!-- Example content -->
+<div class="mt-10 text-center">
+  <p class="text-lg">Your real-time chat platform</p>
+</div>
